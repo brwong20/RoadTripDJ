e0ce4c501c6dfeeb8e6a3fbe84a0150a80b71d23
Brian <brwong20@g.ucla.edu>
2016-11-12 15:17:48 -0800

    Integrated MPMusicPlayer

--- RoadTripDJ/ViewController.m
+++ RoadTripDJ/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@import MediaPlayer;
+
+@interface ViewController ()<MPMediaPickerControllerDelegate>
+
+@property (nonatomic, strong) MPMediaItemCollection *playlist;
+@property (nonatomic, strong) MPMusicPlayerController *player;
+@property (weak, nonatomic) IBOutlet UIToolbar *playerBar;
 
 @end
 
@@ -16,14 +22,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.player = [[MPMusicPlayerController alloc]init];
+    
+}
+
+- (void)togglePlayPause{
+    if(self.player.playbackState == MPMusicPlaybackStatePlaying){
+        [self.player pause];
+    }else{
+        [self.player play];
+    }
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - IBActions
+- (IBAction)addMusic:(id)sender {
+    MPMediaPickerController *mediaPicker = [[MPMediaPickerController alloc]initWithMediaTypes:MPMediaTypeMusic];
+    mediaPicker.delegate = self;
+    mediaPicker.prompt = @"Add Music";
+    mediaPicker.showsCloudItems = YES;
+    mediaPicker.allowsPickingMultipleItems = YES;
+    [self presentViewController:mediaPicker animated:nil completion:nil];
 }
 
+- (IBAction)playPause:(id)sender {
+    [self togglePlayPause];
+}
+
+#pragma mark - MPMediaPickerController Delegate methods
+
+- (void)mediaPicker:(MPMediaPickerController *)mediaPicker didPickMediaItems:(MPMediaItemCollection *)mediaItemCollection{
+
+    //If this isn't a new item collection, append to our old one
+    if(!self.playlist){
+        self.playlist = mediaItemCollection;
+    }else{
+        NSMutableArray *items = [NSMutableArray arrayWithCapacity:self.playlist.count + mediaItemCollection.count];
+        [items addObjectsFromArray:self.playlist.items];
+        [items addObjectsFromArray:mediaItemCollection.items];
+        MPMediaItemCollection *collection = [MPMediaItemCollection collectionWithItems:items];
+        self.playlist = collection;
+    }
+    
+    [self.player setQueueWithItemCollection:self.playlist];
+    if(self.player.playbackState != MPMusicPlaybackStatePlaying){
+        [self.player play];
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)mediaPickerDidCancel:(MPMediaPickerController *)mediaPicker{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
