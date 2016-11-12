328950686a3fe1184c467848c70e3f740cfdd493
Brian <brwong20@g.ucla.edu>
2016-11-12 14:47:51 -0800

    Initial Commit

---
+++ RoadTripDJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RoadTripDJ
+//
+//  Created by Brian Wong on 11/12/16.
+//  Copyright © 2016 Brian Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RoadTripDJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RoadTripDJ
+//
+//  Created by Brian Wong on 11/12/16.
+//  Copyright © 2016 Brian Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
